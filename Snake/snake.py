7411877f447cdb30bd2938584b29f5fffd917c19
Joe <josephomalley135@gmail.com>
2021-11-05 04:15:18 -0500

    added prepetual motion

--- Snake/snake.py
+++ Snake/snake.py
@@ -1,4 +1,4 @@
-import pygame, sys, random
+import pygame, sys, random, time
 from pygame.math import Vector2
 
 
@@ -29,28 +29,36 @@ class SNAKE:
         self.x = 0
         self.y = 0
         self.pos = Vector2(self.x, self.y)
+        self.dirc = "right"
         
-
     def spawn_snake(self):
-
             snake_rect = pygame.Rect(self.pos.x * CELL_SIZE, self.pos.y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
             pygame.draw.rect(surface, (0,255,0), snake_rect)
-
-    def move_snake(self, x, y):
-        if 0 <= self.pos.x + x <= 19 and 0 <= self.pos.y + y <= 19:
-            snake.pos += Vector2(x, y)
             
-        else: 
-            print("OUT OF BOUNDS")
-        print(self.pos.y + y)
-    
+    def move_snake(self):
 
-
-    
-    
+        
+        if self.dirc == "up":
+            if 0 <= self.pos.x + 0 <= 19 and 0 <= self.pos.y + -1 <= 19:
+                snake.pos += Vector2(0, -1)
+        elif self.dirc == "down":
+            if 0 <= self.pos.x + 0 <= 19 and 0 <= self.pos.y + 1 <= 19:
+                snake.pos += Vector2(0, 1)
+        elif self.dirc == "left":
+            if 0 <= self.pos.x + -1 <= 19 and 0 <= self.pos.y + 0 <= 19:
+                snake.pos += Vector2(-1, 0)
+        elif self.dirc == "right":
+            if 0 <= self.pos.x + 1 <= 19 and 0 <= self.pos.y + 0 <= 19:
+                snake.pos += Vector2(1, 0)
+        
+        
     
 fruit = FRUIT()
 snake = SNAKE()
+
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE,150)
+
 pygame.display.set_caption("Python Snake")
 
 
@@ -60,20 +68,23 @@ while on:
         if e.type == pygame.QUIT:
             print(f"{e.type} event... which is == {pygame.QUIT}")
             on = False
+        if e.type == SCREEN_UPDATE:
+            snake.move_snake()
+
         if e.type == pygame.KEYDOWN:
             print("key")
             if e.key == pygame.K_w:
                 print("trying to go up")
-                snake.move_snake(0, -1)  
+                snake.dirc = "up"  
             if e.key == pygame.K_s:
                 print("trying to go down")
-                snake.move_snake(0, 1) 
+                snake.dirc = "down"
             if e.key == pygame.K_a:
                 print("trying to go left")
-                snake.move_snake(-1, 0) 
+                snake.dirc = "left" 
             if e.key == pygame.K_d:
                 print("trying to go right")
-                snake.move_snake(1, 0)   
+                snake.dirc = "right"   
     surface.fill(color)
     fruit.draw_fruit()
     snake.spawn_snake()
